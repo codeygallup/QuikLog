7664ef653156a7acd43c90bdc0c809a9233fffb1
Codeyg12 <codey.gallup@gmail.com>
2025-10-03 18:15:36 -0600

    Simple logging added

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { getLogTemplate } from "./logTemplate";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "quiklog" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "quiklog" is now active!');
+  // Command: insert a formatted log for the currently selected variable or the word at cursor
+  const insertLogDisposable = vscode.commands.registerCommand(
+    "quiklog.insertLog",
+    async () => {
+      const editor: vscode.TextEditor | undefined =
+        vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No active editor");
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('quiklog.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from QuikLog!');
-	});
+      const document = editor.document;
+      const selection = editor.selection;
+      let variable = document.getText(selection).trim();
 
-	context.subscriptions.push(disposable);
+      if (!variable) {
+        vscode.window.showInformationMessage(
+          "Please highlight a variable to insert a QuikLog."
+        );
+        return;
+      }
+
+      const language = document.languageId;
+      const template = getLogTemplate(language, variable);
+
+      // Insert the template on the next line after the current selection
+      const lineEnd: vscode.Position = document.lineAt(selection.end.line).range
+        .end;
+      await editor.edit((editBuilder) => {
+        editBuilder.insert(lineEnd, "\n" + template);
+      });
+    }
+  );
+
+  context.subscriptions.push(insertLogDisposable);
 }
 
 // This method is called when your extension is deactivated
--- src/logTemplate.ts
+++ src/logTemplate.ts
@@ -1,23 +1,23 @@
 const getLogTemplate = (language: string, variable: string): string => {
-    let template;
-    switch (language) {
-        case "javascript":  
-        case "typescript":
-            template = `console.log('${variable}: ', ${variable});`;
-            break;
-        case "java":
-            template = `System.out.println("${variable}: " + ${variable});`;
-            break;
-        case "python":
-            template = `print('${variable}: ', ${variable})`;
-            break;
-        case "feature":
-            template = `print '${variable}: ', ${variable}`;
-            break;
-        default:
-            template = `${variable}`;
-    }
-    return template;
-}
+  let template;
+  switch (language) {
+    case "javascript":
+    case "typescript":
+      template = `console.log('${variable}: ', ${variable});`;
+      break;
+    case "java":
+      template = `System.out.println("${variable}: " + ${variable});`;
+      break;
+    case "python":
+      template = `print('${variable}: ', ${variable})`;
+      break;
+    case "feature":
+      template = `print '${variable}: ', ${variable}`;
+      break;
+    default:
+      template = `${variable}`;
+  }
+  return template;
+};
 
 export { getLogTemplate };
